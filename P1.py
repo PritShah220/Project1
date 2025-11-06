8808de39698db24d1c7fd432f217f57350240fe5
Prit PankajBhai Shah <shahprit05@gmail.com>
2025-11-06 14:32:43 +0530

    Add files via upload
    This is A Python Program in this One Project there are some next programs code can be writen in this file

---
+++ P1.py
@@ -0,0 +1,172 @@
+
+# p1 #Write a Programs That Stores 5 Variables & Data Types  
+# name  = " Prit Pankajbhai shah"   
+# age = 21
+# add = "Samdi Chakla Sojitra Maghrola No Khaccho "
+# Roll = 120 
+# College = "Anand Commerce College (ACC)" 
+
+# print("Name:",name)  
+# print("Age:", age)
+# print("Add:",add) 
+# print("Roll",Roll) 
+# print("College:",College)
+
+
+# p2 #Swap two variables without using a third variable.
+
+# a = 5
+# b = 10
+# print("Before swapping:", a, b)
+# a, b = b, a
+# print("After swapping:", a, b)
+
+
+# p3 #Take input from user And Print its Type and Sum 
+# a = int(input("Enter No1")) 
+# b = float (input("Enter No2"))
+
+# print(a+b) 
+# print(type(a))
+# print(type(b)) 
+
+# p4 #Single line comment program 
+#  This is a single-line comment
+# print("Hello World")  # Inline comment
+
+# This is a multi-line comment
+# spanning several lines
+
+# print("Hello World")
+# '''
+# This is a multi-line string
+# often used as a comment
+# '''
+# print("Hello World")
+
+# p5 #Create variables in camelCase, snake_case, and PascalCase.
+
+# Username = "Pritshah"
+
+# user_name = "Dragon" 
+
+# class UserAccount:
+#     def __init__(self):    
+#         self.UserName = "print"
+#         print(username) 
+#         print(user_name)
+#         User = UserAccount() 
+#         print(user.UserName) 
+
+#p6 . find type of variable using type function 
+# name = "prit"
+# print(type(name)) 
+
+# p7.convert Float to Int value
+
+# float_value = 9.3
+# int_value = int(float_value) 
+# print(float_value) 
+
+#p8. Generating the location of variable
+# x= 56
+# print(id(x)) 
+# print(hex(x))
+
+
+ 
+
+#p9.Take user input for height (float) and weight (int). Print both.
+
+# height = float(input("Enter No1")) 
+# weight = int(input("Enter No2"))
+# print("height is",height) 
+# print("weight is",weight)        
+        
+#p10 .Perform Following Operation of Area of Circle 
+
+# import math
+
+# # Ask the user to input the radius
+# radius = float(input("Enter the radius of the circle: "))
+
+# # Calculate the area using the formula area = π * r^2
+# area = math.pi * radius ** 2
+
+# # Print the result
+# print(f"The area of the circle with radius {radius} is: {area:.2f}")
+
+
+
+#p11.Convert Celsius to Fahrenheit.
+
+# Cel = float(input("Enter Temprature in Celsius")) 
+
+# fahrenheit = (Cel * 9/5)+ 32
+# print(f" degree Celsius is equal to {fahrenheit:.2f} degree Fahrenheit")  
+
+#p12. Fil  out Details of Any Movie
+
+# Movie 1
+# Movie = "Bahubali"
+# Director = "s.s Rajamauli"
+# ReleaseDate = 7,"july",2015
+# About = "Bahubali Movie is About from 2 Brothers"
+# casts = "Prabhas, Rana, Tamanaah, Sathyaraj, Anushka, RamaKrishna , Nassar" 
+# Villain = "prabhakar" 
+
+# print("Name of movie  is",Movie)
+# print("Name of Director  is",Director)
+# print("Date is",ReleaseDate)
+# print("its about",About)
+# print("casting movie",casts) 
+# print("kalkeya is",Villain) 
+
+#13.Add String & Int values in Using Type Casting 
+
+# num_string = "23"
+# num_float = 5.88
+ 
+# num_string = int(num_string)
+# num_sum = num_string+num_float
+# print(num_sum) 
+
+#14.create a Constant variable in UpperCase 
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+        
+        
+        
+
+
+
+     
+
+
+
+
+
+
+
+
