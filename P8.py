18508b3c8d82769094a1fdcd769211b50e089c3f
Prit PankajBhai Shah <shahprit05@gmail.com>
2025-11-16 21:36:47 +0530

    Add files via upload
    this programs has based on tuple operations

---
+++ P8.py
@@ -0,0 +1,20 @@
+#1.Create a tuple of 5 elements.
+
+tup = (10,20,50,60,100)
+print(tup) 
+
+#2.Access elements using index.
+
+a1 = (10,20,30,40,50,60,100)
+print(a1[0:6]) 
+
+#3.Find length of tuple.
+
+Tuple =(10,20,30,40,50,60,100)
+print(len(Tuple))
+
+#4.Count occurrences of a value.
+numbers= (10,20,30,40,50,60)  
+count_10 = numbers.count(10)
+
+print("Occurrences of 10:", count_10)
